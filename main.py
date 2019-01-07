147e191c5dfa8a3f416e420c8de30e5226fa6215
ia03 <ibrahimamsar@gmail.com>
2019-01-06 19:07:46 -0500

    remove redundant comment files

--- main.py
+++
@@ -1,8 +0,0 @@
-# Initialize the bot object.
-# When the bot is ready, load the backup file and print to the console.
-# When a message is sent, check if it is a command.
-# Check if it is the "start" command.
-# Check if it is the "end" command.
-# Check if it is the "changeprefix" command.
-# Check if it is a move.
-# Run the bot.
